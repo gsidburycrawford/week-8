74e197dee3a671887dd97dae15e2446a73d99cef
gsidburycrawford <geterry12345@gmail.com>
2021-04-02 14:49:23 -0400

    Updated about-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeesComponent } from './employees/employees.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+
 
 const routes: Routes = [
   {path: 'employees', component: EmployeesComponent },
   {path: 'homepage', component: HomepageComponent },
+  {path: 'about-page', component: AboutPageComponent},
   {path: '', redirectTo: '/homepage', pathMatch: 'full'}
 ];
 
